6e71ceefc88013841ad2b791762fbf7b57906cab
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-12 15:23:25 +0530

    Create greatest.c

---
+++ greatest.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+ int n1,n2,n3;
+ printf("enter the three numbers");
+ scanf("%d %d %d",&n1,&n2,&n3);
+ if(n1>=n2&&n1>=n3)
+  printf("the greast number is  %d",n1);
+  if(n2>=n1&&n2>=n3)
+   printf("the greast number is%d",n2)
+   if(n3>=n1&&n3>=n2)
+    printf("the greast number is%d",n3);
+    return 0;
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+ 
