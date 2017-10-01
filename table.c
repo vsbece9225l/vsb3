fa1c4cd6f7b8426c0ba935259a427801b226dbe4
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-10-01 10:37:54 -0700

    Update table.c

--- table.c
+++ table.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main()
 {
-int i=1;
-while(i<=10)
+int i=1,n;
+ printf("enter the number:");
+ scanf("%d",&n);
+while(i<=n)
 {
- printf("10*%d=%d\n",i*10);
+ printf("n*%d=%d\n",i*10);
  }
  return 0;
  }
