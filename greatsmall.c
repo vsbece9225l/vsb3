bb75b16249db1349a74ac5c58e443b0c11274a4d
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-10-03 10:45:33 -0700

    Create greatsmall.c

---
+++ greatsmall.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+ int a[5]={23,10,56,8,90};
+ printf("the entered number is:");
+ int i;
+ int greatest=a[0];
+ int smallest=a[0];
+ for(i=0;i<=4;i++)
+ {
+ if(a[i]>biggest}
+ greatest=a[i];
+ }
+ else if(a[i]<smallest)
+ {
+  smallest=a[i];
+  }
+  }
+  printf("the smallest number in the array is %d",smallest);
+  printf("the greast number in the array is %d",greatest);
+  return 0;
+  ]
+  }
