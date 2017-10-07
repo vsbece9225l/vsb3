f2e696f6cbd46355c812ba756a3027dcdd4970ad
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-10-07 08:59:59 -0700

    Create pointeradd.c

---
+++ pointeradd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int *a,*b,sum;
+ printf("enter the two numbers:");
+ scanf("%d%d",&c,&d);
+ a=&c;
+ b=&d;
+ sum=*a+*b;
+ printf("the sum of two number is %d",sum);
+ return 0;
+ }
+ 
+ 
