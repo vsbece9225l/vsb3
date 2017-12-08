d2fbe1146813860f41b3adcb9b16dc9120ab58e1
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-12-07 22:41:15 -0800

    Create name.c

---
+++ name.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+ char name[50]="buttowski";
+ charname1[50];
+ int i=0,j=0;
+ char ch='i';
+ while(name[i]!=ch)
+ {
+  ch1=name[i];
+  name1[j]=ch1;
+  i++;
+  j++;
+  }
+  name1[j]='\0';
+  printf("the entered name buttowski to buttowsk %s",name1);
+  return 0;
+  }
+ 
+ 
+ 
